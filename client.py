e569534a98486ec6b02ce4a8e7c14b28bedfb6e0
RazGavrieli <90526270+RazGavrieli@users.noreply.github.com>
2022-02-16 16:36:20 +0200

    first gui. can send msg, set nickname, click get users button. Server sends disconnected msgs

--- client.py
+++ client.py
@@ -1,36 +1,99 @@
 import threading
 from socket import *
 from _thread import *
+from tkinter import *
+from functools import partial
 
-ClientMultiSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket = socket(AF_INET, SOCK_STREAM)
 host = '127.0.0.1'
 port = 55000
 print('Waiting for connection response')
 try:
-    ClientMultiSocket.connect((host, port))
+    clientSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
 print("connected")
-# res = ClientMultiSocket.recv(1024)
+# res = clientSocket.recv(1024)
 
-def listen(ClientMultiSocket):
+
+def listen(clientSocket, chat):
     connected = True
     while connected:
-        res = ClientMultiSocket.recv(1024)
+        res = clientSocket.recv(1024)
         if not res:
             connected = False
+        chat.config(state=NORMAL)
+        chat.insert("end", res.decode('utf-8'))
+        chat.config(state=DISABLED)
         print(res.decode('utf-8'))
 
 
-Input = input('enter nickname: ')
-ClientMultiSocket.send(str.encode(Input))
+def send(clientSocket, msg, text, chat):
+    if msg is None:
+        clientSocket.send(str.encode("set_msg_all"))
+        msg = text.get("1.0", "end - 1 chars")
+    elif msg is "nickname":
+        msg = text.get("1.0", "end - 1 chars")
+        chat.config(state=NORMAL)
+        chat.delete("1.0", "end")
+        chat.config(state=DISABLED)
+        button2.config(state=DISABLED)
+        button1.config(state=NORMAL)
+        button3.config(state=NORMAL)
 
-start_new_thread(listen, (ClientMultiSocket, ))
-connected = True
-while connected:
-    Input = input('')
-    ClientMultiSocket.send(str.encode(Input))
-    if Input == "disconnect":
-        connected = False
+    clientSocket.send(str.encode(msg))
+    text.delete("1.0", "end")
 
-ClientMultiSocket.close()
+
+root = Tk()
+root.geometry("500x300")
+frame = Frame(root)
+frame.pack()
+chat = Text(frame, undo = True, height = 10, width = 70)
+start_new_thread(listen, (clientSocket, chat))
+leftframe = Frame(root)
+leftframe.pack(side=LEFT)
+
+rightframe = Frame(root)
+rightframe.pack(side=RIGHT)
+
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM)
+
+label = Label(frame, text="Hello world")
+label.pack()
+
+label = Label(frame, text="message: ")
+label.pack()
+
+text = Text(bottomframe, undo=True, height=1, width=40, padx=30, pady=1)
+text.pack(expand=True, fill=BOTH)
+
+chat.pack(fill=BOTH)
+chat.insert("1.0", "enter nickname first\n")
+chat.config(state=DISABLED)
+
+button1 = Button(leftframe, state=DISABLED, text="online users", command=partial(send, clientSocket, "get_users", text, chat))
+button1.pack(padx=3, pady=3)
+button2 = Button(frame, text="enter nickname", command=partial(send, clientSocket, "nickname", text, chat))
+button2.pack(padx=3, pady=3)
+button3 = Button(rightframe, state=DISABLED, text="send", command=partial(send, clientSocket, None, text, chat))
+button3.pack(padx=3, pady=3)
+
+
+
+root.title("chat")
+root.mainloop()
+clientSocket.close()
+
+# Input = input('enter nickname: ')
+# send(clientSocket, Input)
+#
+#
+# connected = True
+# while connected:
+#     Input = input('')
+#     send(clientSocket, Input)
+#     if Input == "disconnect":
+#         connected = False
+
--- server.py
+++ server.py
@@ -8,19 +8,22 @@ msgs = []
 clients = {}
 try:
     serverSocket.bind(addr)
-except socket.error as e:
-    print(str(e))
+except:
+    print("error")
 print('Socket is listening..')
 serverSocket.listen(5)
 
 def newconnection(connection, id):
-
     print(clients)
     data = connection.recv(2048)
+    if not data:
+        print("not data!")
+        exit_thread()
     name = data.decode('utf-8')
     clients[id] = name
-    start_new_thread(receive_req_from_client, (connection, name, id))
     print(name, "connected with id ", id)
+    receive_req_from_client(connection, name, id)
+
 
 def get_users(connection):
     for i in clients.values():
@@ -30,27 +33,31 @@ def receive_req_from_client(connection, name, id):
     start_new_thread(send_msg_to_client, (connection, name, id))
     connected = True
     while connected:
-        data = connection.recv(2048)
-        if not data:
-            print("not data! from ", name, id)
-            connected = False
-        decoded_data = data.decode('utf-8')
-        print("got ", decoded_data, " from ", name, id)
-        if decoded_data == "set_msg_all":
-            print("msg sending", decoded_data)
+        try:
             data = connection.recv(2048)
-            msg = name + ": " + data.decode('utf-8')+"\n"
-            msgs.append(msg)
-        elif decoded_data == "disconnect":
-            connected = False
-        elif decoded_data == "get_users":
-            get_users(connection)
-        else:
-            print("error got weird command:"+ decoded_data)
-
+            if not data:
+                print("not data! from ", name, id)
+                connected = False
+                decoded_data = "disconnect"
+            decoded_data = data.decode('utf-8')
+            print("got ", decoded_data, " from ", name, id)
+            if decoded_data == "set_msg_all":
+                print("msg sending", decoded_data)
+                data = connection.recv(2048)
+                msg = name + ": " + data.decode('utf-8')+"\n"
+                msgs.append(msg)
+            elif decoded_data == "disconnect":
+                connected = False
+            elif decoded_data == "get_users":
+                get_users(connection)
+            else:
+                print("error got weird command:"+ decoded_data)
+        except:
+            break
+    print(id, "disconnected \n")
     clients.pop(id)
     connection.close()
-    msg = name+" disconnected"
+    msg = name+" disconnected\n"
     msgs.append(msg)
 
 
@@ -69,4 +76,4 @@ while True:
     Client, address = serverSocket.accept()
     print('Connected to: ' + address[0] + ':' + str(address[1]))
     id += 1
-    newconnection(Client, id)
+    start_new_thread(newconnection, (Client, id))
