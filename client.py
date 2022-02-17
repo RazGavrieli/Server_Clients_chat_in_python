3fefab2e99bad66b5b1db77ee9783ea08b2369ab
RazGavrieli <90526270+RazGavrieli@users.noreply.github.com>
2022-02-17 14:18:12 +0200

    ver 0.2 added simple GUI, added support for PMs, added doc in readme, added get files

--- client.py
+++ client.py
@@ -5,13 +5,29 @@ from tkinter import *
 from functools import partial
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
-host = '127.0.0.1'
+##############################################################################################################
+win = Tk()
+win.geometry("200x100")
+host = []
+def sethost(host):
+    ip = text.get("1.0", "end - 1 chars")
+    if ip == "":
+        host.append("127.0.0.1")
+    else:
+        host.append(ip)
+    win.destroy()
+text = Text(win, undo=True, height=1, width=40, padx=30, pady=1)
+text.pack(expand=True, fill=BOTH)
+Label(win, text=" enter ip address").pack(pady=20)
+Button(win, text= "connect", command=partial(sethost, host)).pack()
+win.mainloop()
+##############################################################################################################
 port = 55000
 print('Waiting for connection response')
 try:
-    clientSocket.connect((host, port))
-except socket.error as e:
-    print(str(e))
+    clientSocket.connect((host[0], port))
+except:
+    print("error in connecting")
 print("connected")
 # res = clientSocket.recv(1024)
 
@@ -24,20 +40,29 @@ def listen(clientSocket, chat):
             connected = False
         chat.config(state=NORMAL)
         chat.insert("end", res.decode('utf-8'))
+        chat.see("end")
         chat.config(state=DISABLED)
         print(res.decode('utf-8'))
 
 
 def send(clientSocket, msg, text, chat):
     if msg is None:
-        clientSocket.send(str.encode("set_msg_all"))
+        msg = text.get("0.0", "end - 1 chars")
+
+        if msg[0:3] == "!pm" or msg[0:3] == "!PM" or msg[0:3] == "!Pm" or msg[0:3] == "!pM":
+            clientSocket.send(str.encode("set_msg"))
+            msg = msg[3:]
+        else:
+            clientSocket.send(str.encode("set_msg_all"))
+    elif msg == "nickname":
         msg = text.get("1.0", "end - 1 chars")
-    elif msg is "nickname":
-        msg = text.get("1.0", "end - 1 chars")
+        if msg == "" or msg == " " or msg == "   ":
+            msg = "anonymous"
         chat.config(state=NORMAL)
         chat.delete("1.0", "end")
         chat.config(state=DISABLED)
         button2.config(state=DISABLED)
+        button0.config(state=NORMAL)
         button1.config(state=NORMAL)
         button3.config(state=NORMAL)
 
@@ -60,7 +85,7 @@ rightframe.pack(side=RIGHT)
 bottomframe = Frame(root)
 bottomframe.pack(side=BOTTOM)
 
-label = Label(frame, text="Hello world")
+label = Label(frame, text="chat ver 0.2")
 label.pack()
 
 label = Label(frame, text="message: ")
@@ -70,9 +95,12 @@ text = Text(bottomframe, undo=True, height=1, width=40, padx=30, pady=1)
 text.pack(expand=True, fill=BOTH)
 
 chat.pack(fill=BOTH)
-chat.insert("1.0", "enter nickname first\n")
+chat.insert("1.0", "enter your nickname first\n"
+                   "for Private Messages use !pm NAME MSG\n")
 chat.config(state=DISABLED)
 
+button0 = Button(leftframe, state=DISABLED, text="available files", command=partial(send, clientSocket, "get_files", text, chat))
+button0.pack(padx=3, pady=3)
 button1 = Button(leftframe, state=DISABLED, text="online users", command=partial(send, clientSocket, "get_users", text, chat))
 button1.pack(padx=3, pady=3)
 button2 = Button(frame, text="enter nickname", command=partial(send, clientSocket, "nickname", text, chat))
@@ -80,20 +108,8 @@ button2.pack(padx=3, pady=3)
 button3 = Button(rightframe, state=DISABLED, text="send", command=partial(send, clientSocket, None, text, chat))
 button3.pack(padx=3, pady=3)
 
-
-
 root.title("chat")
 root.mainloop()
-clientSocket.close()
 
-# Input = input('enter nickname: ')
-# send(clientSocket, Input)
-#
-#
-# connected = True
-# while connected:
-#     Input = input('')
-#     send(clientSocket, Input)
-#     if Input == "disconnect":
-#         connected = False
+clientSocket.close()
 
--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import threading
 from socket import *
 from _thread import *
+from os import listdir
+from os.path import isfile, join
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 addr = ("127.0.0.1", 55000)
-msgs = []
-clients = {}
+msgs = [] # [list of msgs]
+pms = {} # name, [list of msgs]
+clients = {} # id, its name (online clients only)
 try:
     serverSocket.bind(addr)
 except:
@@ -14,20 +17,44 @@ print('Socket is listening..')
 serverSocket.listen(5)
 
 def newconnection(connection, id):
-    print(clients)
     data = connection.recv(2048)
     if not data:
         print("not data!")
         exit_thread()
     name = data.decode('utf-8')
+    while name in clients.values():
+        name += str(id)
+
     clients[id] = name
     print(name, "connected with id ", id)
+    pms[name] = []
+    msgs.append(name+"["+str(id)+"] connected\n")
     receive_req_from_client(connection, name, id)
 
 
 def get_users(connection):
-    for i in clients.values():
-        connection.sendall(str.encode(i))
+    msg = "---ONLINE USERS---\n"
+    connection.sendall(str.encode(msg))
+    for i in clients:
+        msg = clients.get(i)+"["+str(i)+"]\n"
+        connection.sendall(str.encode(msg))
+    #connection.sendall("\n")
+
+
+def get_files(connection):
+    files = [f for f in listdir("files") if isfile(join("files", f))]
+    if len(files)==0:
+        msg = "---NO FILES AVAILABLE---\n"
+        connection.sendall(str.encode(msg))
+    else:
+        msg = "---AVAILABLE FILES---\n"
+        connection.sendall(str.encode(msg))
+        i = 1
+        for f in files:
+            msg = str(i)+". "+f+"\n"
+            i+=1
+            connection.sendall(str.encode(msg))
+
 
 def receive_req_from_client(connection, name, id):
     start_new_thread(send_msg_to_client, (connection, name, id))
@@ -38,21 +65,48 @@ def receive_req_from_client(connection, name, id):
             if not data:
                 print("not data! from ", name, id)
                 connected = False
-                decoded_data = "disconnect"
             decoded_data = data.decode('utf-8')
             print("got ", decoded_data, " from ", name, id)
-            if decoded_data == "set_msg_all":
+            if decoded_data == "set_msg":
+                data = connection.recv(2048)
+                decoded_data = data.decode('utf-8')
+                splited_msg = decoded_data.split()
+                target = splited_msg[0]
+                decoded_data = decoded_data[len(target)+1:]
+                print("target is "+target)
+                msg = "|PM| " + name + "[" + str(id) + "]: " + decoded_data + "\n"
+                print("msg is "+msg)
+
+                if target in pms.keys():
+                    pms[name].append(msg)
+                    print("entering ")
+                    pms[target].append(msg)
+                elif target.isdecimal():
+                    if int(target) in clients.keys():
+                        pms[name].append(msg)
+                        print("transfuming")
+                        targetemp = clients.get(int(target))
+                        print(targetemp)
+                        pms[targetemp].append(msg)
+                else:
+                    msg = target+" not online\n"
+                    pms[name].append(msg)
+            elif decoded_data == "set_msg_all":
                 print("msg sending", decoded_data)
                 data = connection.recv(2048)
-                msg = name + ": " + data.decode('utf-8')+"\n"
+                msg = name + "["+str(id)+"]: " + data.decode('utf-8')+"\n"
+                print("size is ", len(str.encode(msg)))
                 msgs.append(msg)
             elif decoded_data == "disconnect":
                 connected = False
             elif decoded_data == "get_users":
                 get_users(connection)
+            elif decoded_data == "get_files":
+                get_files(connection)
             else:
                 print("error got weird command:"+ decoded_data)
         except:
+            print("problem occured")
             break
     print(id, "disconnected \n")
     clients.pop(id)
@@ -63,12 +117,19 @@ def receive_req_from_client(connection, name, id):
 
 def send_msg_to_client(connection, name, id):
     prevmsgs = []
+    prevpms = []
     while id in clients.keys():
         if len(msgs)!=len(prevmsgs):
             for i in msgs:
                 if i not in prevmsgs:
                     connection.sendall(str.encode(i))
             prevmsgs = msgs.copy()
+        if pms.get(name) is not None:
+            if len(pms.get(name))!=len(prevpms):
+                for i in pms.get(name):
+                    if i not in prevpms:
+                        connection.sendall(str.encode(i))
+                prevpms = pms.get(name).copy()
 
 
 id = 0
